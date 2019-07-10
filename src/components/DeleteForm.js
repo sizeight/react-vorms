90c8aa64c0f7cd8e37398e60ba1a13e24c37ba32
sizeight <hpretorius@gmail.com>
2019-07-10 09:35:01 +0200

    Remove dependency on reactstrap Button, use plain Bootstrap

--- src/components/DeleteForm.js
+++ src/components/DeleteForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Button, Modal, ModalBody } from 'reactstrap';
+import { Modal, ModalBody } from 'reactstrap';
 
 
 const propTypes = {
