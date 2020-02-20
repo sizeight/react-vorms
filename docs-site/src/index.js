3d914eb4a29e454ef4dc6b1f4d6a7794b4e9b20c
sizeight <hpretorius@gmail.com>
2020-02-20 16:31:13 +0200

    Issue #21 docs indicate maxFileSize

--- docs-site/src/index.js
+++ docs-site/src/index.js
@@ -429,6 +429,7 @@ const Demo = () => {
                         helpText: 'A photo of the plant in it\'s natural environment.',
                         validation: {
                           required: true,
+                          maxFileSize: 5
                         },
                       },
                     ],
