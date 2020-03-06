63ef39daf99ac1f2feca69ae775e4e6c05b70a08
sizeight <hpretorius@gmail.com>
2020-03-06 11:07:32 +0200

    Include FilterForm and FormButtons in lightweight version

--- src/indexLight.js
+++ src/indexLight.js
@@ -1,8 +1,12 @@
 import CustomFormLight from './components/CustomFormLight';
+import FilterForm from './components/FilterForm';
+import FormButtons from './components/FormButtons';
 
 import useReactVorm from './hooks/useReactVorm';
 
 export {
   CustomFormLight as CustomForm,
+  FilterForm,
+  FormButtons,
   useReactVorm,
 };
